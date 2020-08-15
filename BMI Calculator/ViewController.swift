e490a1749a9e80eeaac075da786d51af656d2bd5
tmnabeel30 <tmnabeel30@gmail.com>
2020-08-16 02:11:23 +0530

    UI Completed

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Nabeel on 16/08/20.
+//  Copyright © 2020 Nabeel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
